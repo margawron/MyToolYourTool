314f35c69763245f2fe6e98fc050447d68a6a455
margawron <max2.marcel@gmail.com>
2020-02-09 19:56:03 +0100

    Add Search Form data model.

---
+++ src/main/java/pl/polsl/inzoprog/myToolYourTool/models/forms/SearchForm.java
@@ -0,0 +1,17 @@
+package pl.polsl.inzoprog.myToolYourTool.models.forms;
+
+/**
+ * @author Marcel Gawron
+ * @version 1.0
+ */
+public class SearchForm {
+    String search;
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+}
