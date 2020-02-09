016beadf76833e968c36bf99037d4678596cd5a1
margawron <max2.marcel@gmail.com>
2020-02-09 20:19:11 +0100

    Create email form data model for password recovery.

---
+++ src/main/java/pl/polsl/inzoprog/myToolYourTool/models/forms/EmailForm.java
@@ -0,0 +1,18 @@
+package pl.polsl.inzoprog.myToolYourTool.models.forms;
+
+/**
+ * @author Marcel Gawron
+ * @version 1.0
+ */
+public class EmailForm {
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
