e0784aafcec5540166f0a7eb53831f685679df5e
margawron <max2.marcel@gmail.com>
2020-02-13 21:30:42 +0100

    Add single image to entity.

--- src/main/java/pl/polsl/inzoprog/myToolYourTool/services/SearchService.java
+++ src/main/java/pl/polsl/inzoprog/myToolYourTool/services/SearchService.java
@@ -2,6 +2,7 @@ package pl.polsl.inzoprog.myToolYourTool.services;
 
 import org.springframework.stereotype.Service;
 import pl.polsl.inzoprog.myToolYourTool.models.forms.SearchForm;
+import pl.polsl.inzoprog.myToolYourTool.models.orm.Image;
 import pl.polsl.inzoprog.myToolYourTool.models.orm.Offer;
 
 import javax.persistence.EntityManager;
@@ -14,17 +15,25 @@ import java.util.List;
 public class SearchService {
 
     private EntityManagerFactory entityManagerFactory;
+    private OfferService offerService;
 
-    public SearchService(EntityManagerFactory entityManagerFactory) {
+    public SearchService(EntityManagerFactory entityManagerFactory, OfferService offerService) {
         this.entityManagerFactory = entityManagerFactory;
+        this.offerService = offerService;
     }
 
     public List<Offer> getResults(SearchForm searchForm){
         EntityManager em = entityManagerFactory.createEntityManager();
         String advertisement = searchForm.getSearch();
-        return em.createQuery("select o from Offer o where o.description like :custName", Offer.class)
+        List<Offer> offerList =  em.createQuery("select o from Offer o where o.description like :custName", Offer.class)
                 .setParameter("custName","%"+advertisement+"%")
                 .setMaxResults(10)
                 .getResultList();
+        // Add images
+        for(Offer offer: offerList){
+            List<Image> images = offerService.getSingleImageOfOffer(offer.getId());
+            offer.setOfferImages(images);
+        }
+        return offerList;
     }
 }
